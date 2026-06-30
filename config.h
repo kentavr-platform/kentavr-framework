12552d1a18206f136cb5224afa8a50658b3f99ee
Anton Rezanov <ton@specadmin.ru>
2026-06-30 18:47:57 +0300

    Split core.h and config.h

---
+++ config.h
@@ -0,0 +1,59 @@
+/*------------------------------------------------------------------------------------------------
+ * config.h
+ *
+ *  This file contains project's configuration
+ *
+//----------------------------------------------------------------------------------------------*/
+#ifndef CONFIG_H
+#define CONFIG_H
+//------------------------------------------------------------------------------------------------
+/** Enable and configure hardware UARTs here
+
+    All UART instances share the same driver implementation.
+
+    Warning: buffer sizes must be power of 2 and <= 256 (8-bit optimized mode). Please note,
+    that effective ring buffer capacity is one byte less than the configured buffer size
+    (BUFFER_SIZE - 1).
+
+    A buffer size of 0 (zero) completely disables RX or TX respectively - no interrupt,
+    no buffer, no code.
+
+    Flow control is not implemented in this design. However you can do it yourself.
+
+ ENABLE_UARTx(
+              RX_BUFFER_SIZE,  -- size of reception buffer (required)
+              TX_BUFFER_SIZE,  -- size of transmission buffer (required)
+             )
+
+    Feel free to uncomment and modify the following lines to enable hardware UARTs.
+    See AVR/UART for details.
+                                                                                                */
+ENABLE_UART0(0, 128);
+//ENABLE_UART1(0, 128);
+//------------------------------------------------------------------------------------------------
+/**
+    Enable settings instance and reserves EEPROM space.
+
+    Allocates an EEPROM object of exactly Size bytes and creates a
+    global settings storage instance using that region.
+
+ ENABLE_SETTINGS(SETTINGS_SIZE);
+                 SETTINGS_SIZE  -- EEPROM size reserved for settings storage
+                                   (header + data + index)
+
+
+    Uncomment and modify the following lines to enable it.
+    See core/settings for details.
+                                                                                                */
+//ENABLE_SETTINGS(512);
+
+//------------------------------------------------------------------------------------------------
+/**
+    Enable and configure I2C busses here
+    Feel free to uncomment and modify the following lines to enable hardware I2Cs.
+    See AVR/I2C for details.
+                                                                                                */
+//ENABLE_I2C0(32);
+//ENABLE_I2C1(32);
+//------------------------------------------------------------------------------------------------
+#endif
--- core.h
+++ core.h
@@ -39,51 +39,7 @@
 #include "sensors/DS18S20/DS18S20.h"
 #include "sensors/DS18B20/DS18B20.h"
 //------------------------------------------------------------------------------------------------
-
 /// Early init could be useful in some cases
 void early_init() __attribute__((naked, used, section(".init0")));
-
-//------------------------------------------------------------------------------------------------
-
-/** Enable and configure hardware UARTs here
-
-    All UART instances share the same driver implementation.
-
-    Warning: buffer sizes must be power of 2 and <= 256 (8-bit optimized mode). Please note,
-    that effective ring buffer capacity is one byte less than the configured buffer size
-    (BUFFER_SIZE - 1).
-
-    A buffer size of 0 (zero) completely disables RX or TX respectively - no interrupt,
-    no buffer, no code.
-
-    Flow control is not implemented in this design. However you can do it yourself.
-
- ENABLE_UARTx(
-              RX_BUFFER_SIZE,  -- size of reception buffer (required)
-              TX_BUFFER_SIZE,  -- size of transmission buffer (required)
-             )
-
-    Feel free to uncomment and modify the following lines to enable hardware UARTs.
-                                                                                                */
-// ENABLE_UART0(0, 128);
-// ENABLE_UART1(8, 8);
-
-//------------------------------------------------------------------------------------------------
-
-/**
-    Enable settings instance and reserves EEPROM space.
-
-    Allocates an EEPROM object occupying exactly Size bytes and creates a
-    global settings storage instance using that region.
-
- ENABLE_SETTINGS(
-                 SETTINGS_SIZE  -- EEPROM size reserved for settings storage
-                                   (header + data + index)
-                )
-
-    Uncomment and modify the following lines to enable
-                                                                                                */
-ENABLE_SETTINGS(512);
-
 //------------------------------------------------------------------------------------------------
 #endif
